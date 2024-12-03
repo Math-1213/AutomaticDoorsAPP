816acc92bcb6770f1696cd0d175d90151731f9bb
Math-1213 <matheusprudente9@gmail.com>
2024-12-03 08:45:03 -0300

    Initial Commit

--- app/src/main/java/com/example/automaticdoorsapk/UserActivity.kt
+++ app/src/main/java/com/example/automaticdoorsapk/UserActivity.kt
@@ -1,17 +1,15 @@
 package com.example.automaticdoorsapk
-package com.example.loginapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.automaticdoorsapk.ui.LoginScreen
+import com.example.automaticdoorsapk.userInterface.UserScreen
 
-class MainActivity : ComponentActivity() {
+class UserActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginScreen()  // Tela de Login
+            UserScreen()  // Tela do Usuário
         }
     }
 }
