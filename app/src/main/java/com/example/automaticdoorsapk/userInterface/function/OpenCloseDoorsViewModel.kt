2b488c008f191b91becb05f2b6acc1a4bda8b0d6
Matheus Felipe Prudente <matheusprudente9@gmail.com>
2025-01-08 14:21:47 -0300

    Update OpenCloseDoorsViewModel.kt
    Integrado com o MQTT

--- app/src/main/java/com/example/automaticdoorsapk/userInterface/function/OpenCloseDoorsViewModel.kt
+++ app/src/main/java/com/example/automaticdoorsapk/userInterface/function/OpenCloseDoorsViewModel.kt
@@ -3,8 +3,9 @@ package com.example.automaticdoorsapk.userInterface.function
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.automaticdoorsapk.network.MqttManager
 
-class OpenCloseDoorsViewModel : ViewModel() {
+class OpenCloseDoorsViewModel(private val mqttManager: MqttManager) : ViewModel() {
 
     // Estado do nome do usuário
     private val _userName = MutableLiveData<String>()
@@ -22,20 +23,43 @@ class OpenCloseDoorsViewModel : ViewModel() {
         _userName.value = name
     }
 
+    // Funções para abrir e fechar portas via MQTT
+    fun openInternalDoor() {
+        mqttManager.publish("home/doors/openIN", "")
+        _isInternalDoorOpen.value = true
+        _isExternalDoorOpen.value = false // Garante que a outra porta seja fechada
+    }
+
+    fun closeInternalDoor() {
+        mqttManager.publish("home/doors/closeIN", "")
+        _isInternalDoorOpen.value = false
+    }
+
+    fun openExternalDoor() {
+        mqttManager.publish("home/doors/openOUT", "")
+        _isExternalDoorOpen.value = true
+        _isInternalDoorOpen.value = false // Garante que a outra porta seja fechada
+    }
+
+    fun closeExternalDoor() {
+        mqttManager.publish("home/doors/closeOUT", "")
+        _isExternalDoorOpen.value = false
+    }
+
     // Funções para alternar o estado das portas
     fun toggleInternalDoor() {
-        _isInternalDoorOpen.value = _isInternalDoorOpen.value?.not()
-        // Caso a porta interna seja aberta, fechamos a externa
         if (_isInternalDoorOpen.value == true) {
-            _isExternalDoorOpen.value = false
+            closeInternalDoor()
+        } else {
+            openInternalDoor()
         }
     }
 
     fun toggleExternalDoor() {
-        _isExternalDoorOpen.value = _isExternalDoorOpen.value?.not()
-        // Caso a porta externa seja aberta, fechamos a interna
         if (_isExternalDoorOpen.value == true) {
-            _isInternalDoorOpen.value = false
+            closeExternalDoor()
+        } else {
+            openExternalDoor()
         }
     }
 }
